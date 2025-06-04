8649b638f22a1224d678f467a10145cb1b631b37
Stanley SERVICAL <32981008+stanlee974@users.noreply.github.com>
2025-06-04 17:19:45 +0200

    fix: adapter netlify

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: PROCESS.ENV.ADAPTER === 'netlify' ? adapterNetlify() : adapterVercel()
+		adapter: process.env.ADAPTER === 'netlify' ? adapterNetlify() : adapterVercel()
 	},
 	build: {
 		sourcemap: "hidden",
